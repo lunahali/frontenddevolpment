1532c942cc0ab7e6202abd9e57c58803e19d4051
lunahali <90041321+lunahali@users.noreply.github.com>
2021-09-30 12:48:47 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,22 @@
 // JavaScript Document
+
+var dePlaceholder
+
+dePlaceholder = document.querySelector("placeholder");
+
+dePlaceholder.addEventListener("click", )
+
+function veranderTekst() {
+    dePlaceholder.classList.add("anders")
+}
+
+const hamburger = document.querySelector(".hamburger-menu");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
